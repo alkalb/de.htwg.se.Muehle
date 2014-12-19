cada6d060061b8511364d9cd98825dfa48902b49
alkalb <alkalb@htwg-konstanz.de>
2014-12-19 16:22:34 +0100

    hia

---
+++ src/de/htwg/se/Muehle/Model/Impl/Position.java
@@ -0,0 +1,13 @@
+package de.htwg.se.Muehle.Model.Impl;
+
+public class Position {
+ private int position;
+
+ public Position(int position){
+ this.position = position;
+ }
+
+ public int getPosition(){
+ return position;
+ }
+}
