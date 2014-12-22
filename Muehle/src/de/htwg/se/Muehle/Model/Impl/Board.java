e72da69ca872408772481c34541434c488189b8f
alkalb <alkalb@htwg-konstanz.de>
2014-12-22 16:02:31 +0100

    Hi

---
+++ Muehle/src/de/htwg/se/Muehle/Model/Impl/Board.java
@@ -0,0 +1,34 @@
+package de.htwg.se.Muehle.Model.Impl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Board {
+
+ private final int Tokencount = 24;
+ private Token board[];
+ private Map<Integer, ArrayList<Integer>> connections;
+
+ public Board(){
+ board = new Token[Tokencount];
+ /*for(int i = 0; i < board.length; i++){
+	board[i] = new Token(null, null, null);
+	}*/
+
+ connections = new HashMap<Integer, ArrayList<Integer>>();
+ /*for(int i = 0; i < 24; i++){
+	
+	}*/
+ }
+
+ public boolean isPositionEmpty(int x){
+
+ return (board[x] == null);
+
+ }
+
+ public void setPosition(int x, Token t){
+ board[x] = t;
+ }
+}
---
+++ Muehle/src/de/htwg/se/Muehle/Model/Impl/Player.java
@@ -0,0 +1,43 @@
+package de.htwg.se.Muehle.Model.Impl;
+
+
+import java.awt.Color;
+
+public class Player {
+	private String name;
+	private Color color;
+	private int tokenCount;
+	private int placeableTokenCount;
+	private final int maxToken = 9;
+	
+	public Player(String name, Color color){
+		this.name = name;
+		this.color = color;
+		this.tokenCount = 0;
+		this.placeableTokenCount = maxToken;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public Color getColor(){
+		return color;
+	}
+	
+	public int getTokenCount(){
+		return tokenCount;
+	}
+	
+	public int getPlaceableTokenCount(){
+		return placeableTokenCount;
+	}
+	
+	public void setTokenCount(int x){
+		tokenCount = x;
+	}
+	
+	public void setPlaceableTokenCount(int x){
+		placeableTokenCount = x;
+	}
+}
